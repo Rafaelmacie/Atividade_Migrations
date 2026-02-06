f6c608c09431f1808b93ecfd3e9b42df9b17cb65
Rafael Maciel <rafaelmn934@gmail.com>
2026-02-06 20:20:29 -0300

    feat: Criando as tabelas de pedido e itens do pedido, além de criar um relacionamento entre elas

---
+++ database/migrations/2026_02_06_231618_create_itens_pedido.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('itens_pedido', function (Blueprint $table) {
+            $table->id();
+
+            // Aqui está o relacionamento 1:n
+            $table->foreignId('pedido_id')->constrained('pedidos')->cascadeOnDelete();
+            $table->string('produto_nome');
+            $table->integer('quantidade');
+            $table->decimal('preco_unitario', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('itens_pedido');
+    }
+};
